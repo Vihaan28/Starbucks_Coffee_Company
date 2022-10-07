e1722e726c280c34bd11f1e1eab40eb5fc9b4b6d
Vihaan Mittal <86526176+Vihaan28@users.noreply.github.com>
2022-10-07 09:28:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,111 @@
+function navbar_menue(){
+    window.location="Menu.html"
+}
+
+function navbar_rewards(){
+    window.location="Rewards.html"
+}
+
+function navbar_giftcards(){
+    window.location="Gift Cards.html"
+}
+
+function home_page_rewards_btn(){
+    window.location="Rewards.html"
+}
+
+function home_page_gift_cards_btn(){
+    window.location="Gift Cards.html"
+}
+
+function home_page_summer_essentials_btn(){
+    window.location="Summer Essentials.html"
+}
+
+function quick_links_home(){
+    window.location="Home.html"
+}
+
+function navbar_find_a_store(){
+    window.location="Find_a_store.html"
+}
+
+function see_all_featured_gift(){
+    window.location="Featured_Gift_Cards.html"
+}
+
+function see_all_birthday_gift(){
+    window.location="Birthday_Gift_Cards.html"
+}
+
+function see_all_thankyou_gift(){
+    window.location="Thank_You_Gift_Cards.html"
+}
+
+function see_all_appreciation_gift(){
+    window.location="Appreciation_Gift_Cards.html"
+}
+
+function see_all_workplace_gift(){
+    window.location="Workplace_Gift_Cards.html"
+}
+
+function see_all_love_gift(){
+    window.location="Love_Gift_Cards.html"
+}
+
+function hot_coffees_menu(){
+    window.location="Hot_Coffees_Menu.html"
+}
+
+function hot_teas_menu(){
+    window.location="Hot_Teas_Menu.html"
+}
+
+function hot_drinks_menu(){
+    window.location="Hot_Drinks_Menu.html"
+}
+
+function frappuccino_menu(){
+    window.location="Frappuccino_Menu.html"
+}
+
+function cold_coffees_menu(){
+    window.location="Cold_Coffee's_Menu.html"
+}
+
+function iced_teas_menu(){
+    window.location="Iced_Teas_Menu.html"
+}
+
+function cold_drinks_menu(){
+    window.location="Cold_Drinks_Menu.html"
+}
+
+function hot_breakfast_menu(){
+    window.location="Hot_Breakfast.html"
+}
+
+function bakery_menu(){
+    window.location="Bakery.html"
+}
+
+function lunch_menu(){
+    window.location="Lunch.html"
+}
+
+function snacks_and_sweets_menu(){
+    window.location="Snacks_And_Sweets.html"
+}
+
+function oatmeal_and_yogurt_menu(){
+    window.location="Oatmeal_And_Yogurt.html"
+}
+
+function whole_bean_menu(){
+    window.location="Whole_Bean.html"
+}
+
+function VIA_Instant(){
+    window.location="VIA®_Instant.html"
+}
